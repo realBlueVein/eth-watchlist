944dbdd6a2eb68c6d825163d3580be44761a4d4d
James <james@blueve.in>
2021-01-10 20:33:50 -0800

    v1.0 finished

--- contracts/Watchlist.sol
+++ contracts/Watchlist.sol
@@ -12,6 +12,18 @@ contract Watchlist {
 
     mapping(uint => Stock) public stocks;
 
+    event StockCreated(
+        uint id,
+        string content,
+        bool completed
+    );
+
+    event StockCompleted(
+        uint id,
+        bool active
+    );
+
+
     // init that runs on deployment
     constructor() public {
         addStock("TSLA"); // Adding sample data to the watchlist
@@ -21,7 +33,15 @@ contract Watchlist {
     function addStock(string memory _content) public {
         listCount ++;
         stocks[listCount] = Stock(listCount, _content, true);
+        emit StockCreated(listCount, _content, false);
     }
+
+    function toggleCompleted(uint _id) public {
+        Stock memory _stock = stocks[_id];
+        _stock.active = !_stock.active;
+        stocks[_id] = _stock;
+        emit StockCompleted(_id, _stock.active);
+  }
 
 }
 
