a610312ba3f240d7f5a6730028853d3fe1c4df85
James <james@blueve.in>
2021-01-10 19:31:51 -0800

    Server side done

--- contracts/Watchlist.sol
+++ contracts/Watchlist.sol
@@ -3,4 +3,26 @@ pragma solidity ^0.5.0;
 contract Watchlist {
     uint public listCount = 0;
 
+
+    struct Stock {
+        uint id;
+        string ticker;
+        bool active;
+    }
+
+    mapping(uint => Stock) public stocks;
+
+    // init that runs on deployment
+    constructor() public {
+        addStock("TSLA"); // Adding sample data to the watchlist
+    }
+
+
+    function addStock(string memory _content) public {
+        listCount ++;
+        stocks[listCount] = Stock(listCount, _content, true);
+    }
+
 }
+
+//flow --> truffle compile --> truffle migrate --> 
